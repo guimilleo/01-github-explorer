65fd5992d34e25d6572bbf36e1c8ca03d7fd35cb
Guilherme Milléo <guilherme@e-peca.com.br>
2021-03-21 17:28:10 -0300

    fetch api github

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,4 +1,6 @@
+import { useState, useEffect } from 'react';
 import { RepositoryItem } from "./RepositoryItem"
+import '../styles/repositories.scss'
 
 const repository = {
     name: 'Unform',
@@ -6,15 +8,29 @@ const repository = {
     link: 'https://github.com/unform/unform'
 }
 
+// https://api.github.com/orgs/rocketseat/repos
+
 export function RepositoryList() {
+
+    const [repositories, setRepositories] = useState([])
+
+    useEffect(()=>(
+        fetch('https://api.github.com/orgs/rocketseat/repos')
+            .then(response => response.json())
+            .then(data => setRepositories(data))
+    ),[])
+
     return (
         <section className="repository-list">
             <h1>Lista de repositórios</h1>
             <ul>
-               <RepositoryItem repository={repository}/>
-               <RepositoryItem/>
-               <RepositoryItem/>
-               <RepositoryItem/>
+                {
+                    repositories.map(repository => {
+                        return <RepositoryItem key={repository.name} repository={repository}/>
+                    })
+                }
+               
+               
             </ul>
         </section>
     )
